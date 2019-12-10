b9b39d24b72a14062f0a500d75e20d1426f786f6
1703954 <1703954@SC-VDGD-2QQLT52.local>
2019-12-10 09:16:46 -0800

    Update App.config, CRPG.csproj, and 3 more files...

---
+++ CRPG/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Trayden Gerik");
+            Console.ReadKey();
+        }
+    }
+}
